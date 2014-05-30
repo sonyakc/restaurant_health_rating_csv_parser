46ab19ab08a67df96db5fbda70e373b16303ceac
Sonya <sonyakc.2007@gmail.com>
2014-05-29 23:16:35 -0400

    Write ratings data out to mongodb

--- health_rating_parser.rb
+++ health_rating_parser.rb
@@ -1,24 +1,41 @@
 require 'rubygems'
 require 'csv'
 require 'smarter_csv'
+require 'mongo'
+include Mongo
 
 puts Dir.pwd
 
-# iconv -f ISO-8859-1 -t utf-8 WebExtract.txt > Modified.txt
-out_file = File.new("out.csv", "w+")
-File.foreach('WebExtract.txt')  {
-	|f| 
-	newf = f.force_encoding('UTF-8').encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '').gsub(/\"/,'""')
-	out_file.puts newf
-}
-out_file.rewind
-
-begin
-	pets_by_owner = SmarterCSV.process(out_file, col_sep: '""')
-	puts pets_by_owner.class 
-	puts pets_by_owner[0]
-	puts pets_by_owner[5001]
-rescue EOFError
-	puts 'got an error'
+def database_connection
+	client = MongoClient.new # defaults to localhost:27017 db = client[‘example-db’] coll = db[‘example-collection’]
+	db   = client.db 'health' 
+	db.drop_collection 'ratings'
+	ratings_coll = db.create_collection 'ratings'
 end
 
+def parse_ratings_csv
+	ratings_file = File.new("ratings.csv", "w+")
+
+	# iconv -f ISO-8859-1 -t utf-8 WebExtract.txt > Modified.txt
+	File.foreach('WebExtract.txt')  {
+		|f| newf = f.force_encoding('UTF-8').encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '').gsub(/\"/,'""')
+		ratings_file.puts newf
+	}
+	ratings_file.rewind
+
+	# load data to Mongo
+	begin
+		all_ratings = SmarterCSV.process(ratings_file, col_sep: '"",')
+		# puts all_ratings.class Array
+		# all_ratings.each{ |x| ratings_coll.insert x }
+		puts all_ratings[5001]
+	rescue EOFError
+		p 'got an error'
+	end
+	ratings_file.close
+	p "done loading data"
+end
+
+# Usage
+database_connection()
+parse_ratings_csv()
